133cef0e9eb2eb93b3bcfe0bbf1853a2bd4fa271
Evan Bonsignori <evanbonsignori@yahoo.com>
2018-11-29 01:35:18 -0500

    Add REST endpoints and their corresponding db queries for menu items

--- app/db/queries.js
+++ app/db/queries.js
@@ -2,8 +2,42 @@ module.exports = {
     menu: {
         get:
             "SELECT * FROM menu",
+        get_item:
+            "SELECT * FROM menu " +
+            "WHERE menu.id = $1",
         new_item:
             "INSERT INTO menu(item_name, item_price, category, modified_date, created_date) " +
-            "VALUES($1, $2, $3, $4, $5)"
+            "VALUES($1, $2, $3, $4, $5)",
+        remove_item:
+            "DELETE FROM menu" +
+            "WHERE menu.id = $1",
+        edit_item: (id, {
+            item_name = undefined,
+            item_price = undefined,
+            category = undefined,
+            modified_date = new Date()
+        } = {}) => {
+            let insert_string = "INSERT INTO menu(";
+            let insert_args = [];
+            if (item_name) {
+                insert_string += "item_name, ";
+                insert_args.push(item_name)
+            }
+            if (item_price) {
+                insert_string += "item_price, ";
+                insert_args.push(item_price)
+            }
+            if (category) {
+                insert_string += "category, "
+            }
+            insert_args.push(modified_date);
+            insert_string += "modified_date) VALUES(";
+            for (let i = 1; i < insert_args.length; i++) {
+                insert_string += "$" + i;
+            }
+            insert_string += ")";
+
+            return [insert_string, insert_args];
+        }
     },
 };
--- app/routes/menu.js
+++ app/routes/menu.js
@@ -5,23 +5,64 @@ const router = require('express').Router();
 const logger = require("../config/logging");
 
 
-// At /menu, return entire menu
+// Get every menu entry
 router.get("/", async function returnMenu(req, res) {
     const results = await db.query(queries.menu.get);
     res.status(200).json({menu: results.rows});
 });
 
+// Add a new item to the menu TODO: Test and improve cases
 router.post("/add", async function addMenuItem(req, res) {
-
-    const insert_res = await db.query(queries.menu.new_item, [
-        req.query.item_name,
-        req.query.price,
-        req.query.category,
-        new Date(),
-        new Date()
-    ]);
-    console.log(insert_res);
+    try {
+        const insert_res = await db.query(queries.menu.new_item, [
+            req.query.item_name,
+            req.query.price,
+            req.query.category,
+            new Date(),
+            new Date()
+        ]);
+    } catch (err) {
+        logger.error(err);
+        res.status(400);
+    }
     res.status(200);
+});
+
+// Delete a specific item on menu using the item's id TODO: Test and improve cases
+router.delete("/remove", async function addMenuItem(req, res) {
+    const delete_res = await db.query(queries.menu.remove_item, [
+        req.query.item_id
+    ]);
+    res.status(200);
+});
+
+// Edit specific item on menu using the item's id TODO: Test and improve cases
+router.put("/edit", async function editMenuItem(req, res) {
+    const edit_item_query = queries.menu.edit_item(req.query.item_id, {
+        item_name: req.query.item_name ? req.query.item_name : undefined,
+        item_price: req.query.item_price ? req.query.item_price : undefined,
+        category: req.query.category ? req.query.category : undefined,
+    });
+    try {
+        const edit_res = await db.query(edit_item_query[0], edit_item_query[1]);
+    } catch (err) {
+        logger.error(err);
+        res.status(400);
+    }
+    res.status(200);
+});
+
+// Get specific item on menu TODO: Test and improve cases
+router.get("/get/:item_id", async function deleteMenuItem(req, res) {
+    try {
+        const get_res = await db.query(queries.menu.get_item, [
+            req.query.item_id
+        ]);
+    } catch (err) {
+        logger.error(err);
+        res.status(400);
+    }
+    res.status(200).json({bear: get_res.rows[0]});
 });
 
 module.exports = router;
