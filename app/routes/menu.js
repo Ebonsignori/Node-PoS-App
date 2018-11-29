bb7207a453f2ee4171f0f7d93b3489a47d05b102
Evan Bonsignori <evanbonsignori@yahoo.com>
2018-11-29 18:42:39 -0500

    Make menu routes follow RESTful CRUD practices

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -4,15 +4,47 @@ const chalk = require("chalk");
 const router = require('express').Router();
 const logger = require("../config/logging");
 
+// =================================================
+// Routes
+// =================================================
+
+// Return entire menu
+router.route("/")
+    .get(returnMenu);
+
+// RESTfully perform CRUD operations a specific menu item by its id
+router.route("/:item_id", )
+    .get(getSpecificMenuItem)
+    .post(addMenuItem)
+    .delete(deleteMenuItem)
+    .put(editMenuItem);
+
+
+// =================================================
+// Route Logic
+// =================================================
 
 // Get every menu entry
-router.get("/", async function returnMenu(req, res) {
+async function returnMenu(req, res) {
     const results = await db.query(queries.menu.get);
     res.status(200).json({menu: results.rows});
-});
+}
+
+// Get a specific menu item TODO: Test and improve cases
+async function getSpecificMenuItem(req, res) {
+    try {
+        const get_res = await db.query(queries.menu.get_item, [
+            req.query.item_id
+        ]);
+    } catch (err) {
+        logger.error(err);
+        res.status(400);
+    }
+    res.status(200).json({bear: get_res.rows[0]});
+}
 
 // Add a new item to the menu TODO: Test and improve cases
-router.post("/add", async function addMenuItem(req, res) {
+async function addMenuItem(req, res) {
     try {
         const insert_res = await db.query(queries.menu.add_item, [
             req.query.item_name,
@@ -26,18 +58,18 @@ router.post("/add", async function addMenuItem(req, res) {
         res.status(400);
     }
     res.status(200);
-});
+}
 
 // Delete a specific item on menu using the item's id TODO: Test and improve cases
-router.delete("/remove", async function addMenuItem(req, res) {
+async function deleteMenuItem(req, res) {
     const delete_res = await db.query(queries.menu.remove_item, [
         req.query.item_id
     ]);
     res.status(200);
-});
+}
 
 // Edit specific item on menu using the item's id TODO: Test and improve cases
-router.put("/edit", async function editMenuItem(req, res) {
+async function editMenuItem(req, res) {
     const edit_item_query = queries.menu.edit_item(req.query.item_id, {
         item_name: req.query.item_name ? req.query.item_name : undefined,
         item_price: req.query.item_price ? req.query.item_price : undefined,
@@ -50,19 +82,6 @@ router.put("/edit", async function editMenuItem(req, res) {
         res.status(400);
     }
     res.status(200);
-});
-
-// Get specific item on menu TODO: Test and improve cases
-router.get("/get/:item_id", async function deleteMenuItem(req, res) {
-    try {
-        const get_res = await db.query(queries.menu.get_item, [
-            req.query.item_id
-        ]);
-    } catch (err) {
-        logger.error(err);
-        res.status(400);
-    }
-    res.status(200).json({bear: get_res.rows[0]});
-});
+}
 
 module.exports = router;
