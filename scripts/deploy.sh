b2bf2e51d2913ff75fb626ac6983d0dc3d4aea6e
Evan Bonsignori <evanbonsignori@yahoo.com>
2019-01-15 13:16:18 -0500

    Change repo dir & ignore repo changes when pushing to remote from CI VM

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,12 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Add remote to known_hosts
     ssh-keyscan -H 157.230.152.107 >> ~/.ssh/known_hosts
     # Push repo to DigitalOcean server using git
-    git remote add deploy "root@157.230.152.107:/home/deploy/app"
+    git remote add deploy "root@157.230.152.107:/root/app"
     git config user.name "Travis CI"
     git config user.email "evanbonsignori+travisCI@yahoo.com"
+    # Stash changes, then remove the stash (unneeded in CI VM)
+    git stash save --keep-index --include-untracked
+    git stash drop
     git push --force deploy master
 else
     echo "Not deploying, since this branch isn't master."
